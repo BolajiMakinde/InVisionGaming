6e2bd5cfe4de5be56ad4f77240a7f37cc4977af7
Bolaji Makinde <44919483+BolajiMakinde@users.noreply.github.com>
2021-04-03 12:08:56 -0700

    webcam reader working

--- PythonScripts/OCRReader.py
+++ PythonScripts/OCRReader.py
@@ -1,4 +1,5 @@
-# credit goes to https://www.youtube.com/watch?v=4DrCIVS5U3Y AllTech Youtube Channel for Starter Code
+# credit goes to https://www.youtube.com/watch?v=4DrCIVS5U3Y 'AllTech' YouTube Channel for Starter Code
+# credit goes to https://www.youtube.com/watch?v=Jvf5y21ZqtQ 'sentdex' YouTube Channel for webcam support
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 import os.path
@@ -15,8 +16,21 @@ if Screen_Capture == True:
     img = ImageGrab.grab()
 else:
     cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('r'):
+            print('Reading ...')
+            status = cv2.imwrite('reader.png',frame)
+            #print("Image written to file-system : ",status)
+            img = Image.open(os.path.dirname(__file__) + '/../reader.png')
+            text = tess.image_to_string(img)
+            print(text)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 #img = Image.open(os.path.dirname(__file__) + '/../logo.png')
 
 #with picamera.PiCamera() as camera:
