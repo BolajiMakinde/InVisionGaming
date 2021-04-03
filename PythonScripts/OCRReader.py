471853b9e357ea945f502f55c4aaf591b83fe84f
Bolaji Makinde <44919483+BolajiMakinde@users.noreply.github.com>
2021-04-03 11:15:22 -0700

    most basic form of ocr working for one image

--- PythonScripts/OCRReader.py
+++ PythonScripts/OCRReader.py
@@ -1,5 +1,6 @@
 # credit goes to https://www.youtube.com/watch?v=4DrCIVS5U3Y AllTech Youtube Channel for Starter Code
 import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 import os.path
 from PIL import Image
 
