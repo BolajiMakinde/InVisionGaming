03a0003cf82e891a1aa09180e103dea8d5e26051
Bolaji Makinde <44919483+BolajiMakinde@users.noreply.github.com>
2021-04-03 11:46:27 -0700

    screen capture ocr working

--- PythonScripts/OCRReader.py
+++ PythonScripts/OCRReader.py
@@ -2,10 +2,29 @@
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 import os.path
+import time
+#from picamera import Picamera
+import cv2
+import numpy as np
 from PIL import Image
+from PIL import ImageGrab
 
-img = Image.open(os.path.dirname(__file__) + '/../logo.png')
+Screen_Capture = False
 
-text = tess.image_to_string(img)
+if Screen_Capture == True:
+    img = ImageGrab.grab()
+else:
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+#img = Image.open(os.path.dirname(__file__) + '/../logo.png')
 
-print(text)
+#with picamera.PiCamera() as camera:
+#    camera.resolution = (1024, 768)
+#    camera.start_preview()
+#    time.sleep(2)
+#    camera.capture(foo.jpg)
+#img = Image.open(foo.jpg)
+#text = tess.image_to_string(img)
+
+#print(text)
